ea24942a8aa51db2f1a1191af302a83dcc4dad13
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-08-08 19:22:38 +0530

    Local memory Tool Created

--- personal_knowledge_assistant/memory.py
+++ personal_knowledge_assistant/memory.py
@@ -1,6 +1,7 @@
 import os
 import json
-
+from typing import Optional
+from datetime import datetime
 class LocalMemory:
     def __init__(self, file_path = "conversation.data"):
         self.file_path = file_path
@@ -9,9 +10,52 @@ class LocalMemory:
             with open(file_path, "w") as f:
                 json.dump([], f)
 
-    def load_memory(self):
+    def load_memory(self,
+                    timestamp: Optional[str] = None,
+                    limit: Optional[str] = None,
+                    search_term: Optional[str] = None
+                    ):
+        
         with open(self.file_path, "r") as f:
-            return json.load(f)
+            messages =  json.load(f)
+
+        if timestamp:
+            try:
+                target_time = datetime.fromisoformat(timestamp)
+                
+                filtered_messages = []
+                
+                for message in messages:
+                    message_time = datetime.fromisoformat(message["timestamp"])
+                    if len(timestamp) == 10:
+                        if target_time.date() == message_time.date():
+                            filtered_messages.append(message)
+                
+                    else:
+                        if target_time == message_time:
+                            filtered_messages.append(message)
+                                        
+                messages = filtered_messages
+
+            except ValueError:
+                raise ValueError(
+                "Invalid timestamp format. Use YYYY-MM-DD "
+                "or YYYY-MM-DDTHH:MM:SS"
+            )
+
+        if search_term:
+            search_term = search_term.lower()
+
+            messages = [
+                message for message in messages
+                if search_term in message["question"].lower()
+                or search_term in message["answer"].lower()
+            ]
+        
+        if limit:
+            messages = messages[-limit:]
+
+        return messages
     
     def save_memory(self, question, answer, date):
         memory_data = self.load_memory()
