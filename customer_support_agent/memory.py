0c83dcd00434223e5e830c921ad804f0df8aba2e
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-08-17 12:20:15 +0530

    Conversation memory

---
+++ customer_support_agent/memory.py
@@ -0,0 +1,32 @@
+import json
+import os
+from datetime import datetime
+class LocalMemory:
+    def __init__(self, file_path="conversation.data"):
+        self.file_path = file_path
+
+        if not os.path.exists(file_path):
+            with open(file_path, "w") as f:
+                json.dump([], f)
+
+    def load_memory(self):
+        with open(self.file_path, "r") as f:
+            return json.load(f)
+
+    def save_memory(self, User, agent):
+        memory_content = self.load_memory()
+
+        timestamp =  datetime.now().isoformat()
+
+        memory_content.append({
+            "User": User,
+            "Agent": agent,
+            "timestamp": timestamp
+        })
+
+        with open(self.file_path, "w") as f:
+            json.dump(memory_content, f, indent=4)
+
+    def clear_memory(self):
+        with open(self.file_path, "w") as f:
+            json.dump([], f)
