4d982651362ead5dd35fff13bd2e9ce939692e2f
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-08-19 13:05:28 +0530

    Created Tickets table

--- customer_support_agent/DB_Manager/ticketsDB.py
+++ customer_support_agent/DB_Manager/ticketsDB.py
@@ -4,3 +4,17 @@ class TicketDatabaseManager:
     def __init__(self, db_name="customer_support_database.db"):
         self.connection = sqlite3.connect(db_name)
         self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        self.cursor.execute("""
+            create table if not exists tickets(
+            ticket_id TEXT primary key,
+            type TEXT NOT NULL,
+            customer_id TEXT,
+            order_id TEXT,
+            item_id TEXT NOT NULL,
+            status TEXT NOT NULL,
+            description TEXT,
+            created_at TEXT NOT NULL
+            )
+""")
