03d4a0006672ad3d4c66487f94d1cd1fcafc5b93
IamFJ03 <130732739+IamFJ03@users.noreply.github.com>
2026-08-21 21:50:27 +0530

    add table for ticket created

--- customer_support_agent/DB_Manager/ticketsDB.py
+++ customer_support_agent/DB_Manager/ticketsDB.py
@@ -4,6 +4,7 @@ class TicketDatabaseManager:
     def __init__(self, db_name="customer_support_database.db"):
         self.connection = sqlite3.connect(db_name)
         self.cursor = self.connection.cursor()
+        self.create_table()
 
     def create_table(self):
         self.cursor.execute("""
@@ -12,13 +13,20 @@ class TicketDatabaseManager:
             type TEXT NOT NULL,
             customer_id TEXT NOT NULL,
             order_id TEXT,
-            item_id TEXT NOT NULL,
+            item_id INTEGER NOT NULL,
             status TEXT NOT NULL CHECK (
                 status IN ('open', 'in_progress', 'resolved', 'closed')
             ),
             description TEXT,
             created_at TEXT NOT NULL,
         
-            FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+            FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
             );
         """)
+
+    def add_table(self, ticket_id, ticket_type, customer_id, order_id, item_id, status, description, created_at):
+        self.cursor.execute("""
+            Insert into tickets(ticket_id, type, customer_id, order_id, item_id, status, description, created_at) values(?, ?, ?, ?, ?, ?, ?, ?)
+""", (ticket_id, ticket_type, customer_id, order_id, item_id, status, description, created_at))
+
+        self.connection.commit()
